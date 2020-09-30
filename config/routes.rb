1dd24577266973580a0bbce9cc0b275b835ffc6f
adilson carvalho <adilsonsantanadecarvalho@gmail.com>
2020-09-30 17:54:11 -0300

    create evaluations, submitted_texts and translated_texts controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :submitted_text, only [ :index, :edit, :new, :create, :destroy ] do
-    resources :translated_text, only [ :index, :new, :edit, :update] do
-      resources :evaluation, only [:new, :show, :edit, :update]
+  resources :submitted_texts, only: [ :index, :edit, :new, :create, :destroy ] do
+    resources :translated_texts, only: [ :index, :new, :edit, :update ] do
+      resources :evaluations, only: [ :new, :show, :edit, :update ]
+    end
+  end
 end
---
+++ test/controllers/submitted_texts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SubmittedTextsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
