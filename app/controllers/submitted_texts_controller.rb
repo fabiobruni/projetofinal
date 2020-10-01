3e23056d3c9a11a879846468c0bcccc5e6c34eb0
Marcos Flores <marcoschines@gmail.com>
2020-10-01 12:50:45 -0300

    Submitted text show made

--- app/controllers/submitted_texts_controller.rb
+++ app/controllers/submitted_texts_controller.rb
@@ -1,2 +1,10 @@
 class SubmittedTextsController < ApplicationController
+   skip_before_action :authenticate_user!, only: :show
+
+  def show
+    @text = SubmittedText.find(params[:id])
+    if TranslatedText.all.select { |translation| translation.submitted_text_id == @text.id}.size > 0
+      @reviews = TranslatedText.all.select { |translation| translation.submitted_text_id }
+    end
+  end
 end
---
+++ db/migrate/20201001151516_change_target_public_columns_submitted_and_translated.rb
@@ -0,0 +1,7 @@
+class ChangeTargetPublicColumnsSubmittedAndTranslated < ActiveRecord::Migration[6.0]
+  def change
+    change_column :submitted_texts, :target_public, :text
+    change_column :translated_texts, :target_public, :text
+    add_column :submitted_texts, :potential_public, :integer
+    add_column :translated_texts, :potential_public, :intege  end
+end
