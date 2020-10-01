df8e60352b1b10eaa8ce3c69eafb85c75a6b1c7e
Marcos Flores <marcoschines@gmail.com>
2020-10-01 18:20:54 -0300

    adding choose

--- app/controllers/translated_texts_controller.rb
+++ app/controllers/translated_texts_controller.rb
@@ -1,8 +1,10 @@
 class TranslatedTextsController < ApplicationController
   def show
     @review = TranslatedText.find(params[:id])
+    @evaluation = Evaluation.new
     if Evaluation.all.select { |evaluation| evaluation.translated_text_id == @review.id}.size > 0
       @evaluations = Evaluation.all.select { |evaluation| evaluation.translated_text_id }
     end
+    @original = SubmittedText.find(@review.submitted_text_id)
   end
 end
