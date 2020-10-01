cc6e043d005504aeab823f3458860dd6333e8060
adilson carvalho <adilsonsantanadecarvalho@gmail.com>
2020-10-01 12:02:21 -0300

    create validations

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,4 +2,6 @@ class Evaluation < ApplicationRecord
   belongs_to :user
   belongs_to :translated_text
   has_one :submitted_text, through: :translated_text
+  validates :stars, presence: true
+  validates :stars, inclusion: { in: [1,2,3,4,5] }
 end
