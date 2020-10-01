29bbd4501bf2cdcb41a0ac87054aca47b5669131
Fabio Bruni <fabiobruni@gmail.com>
2020-10-01 15:40:55 -0300

    action destroy

--- app/controllers/submitted_texts_controller.rb
+++ app/controllers/submitted_texts_controller.rb
@@ -1,5 +1,8 @@
 class SubmittedTextsController < ApplicationController
-   skip_before_action :authenticate_user!, only: :show
+  before_action :set_text, only: [:destroy]
+   
+  skip_before_action :authenticate_user!, only: :show
+ 
   # GET /submitted_texts
   def index
     @submitted = SubmittedText.all
@@ -10,4 +13,17 @@ class SubmittedTextsController < ApplicationController
       @reviews = TranslatedText.all.select { |translation| translation.submitted_text_id }
     end
   end
+
+  # DELETE /submitted_texts/1
+  def destroy
+    @submitted.destroy
+    redirect_to submitted_texts_url, notice: 'Pronto, seu texto foi apagado.'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_text
+    @submitted = SubmittedText.find(params[:id])
+  end
+
 end
