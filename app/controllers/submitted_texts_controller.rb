9d230a82c04d68ff5ab2c6740db0db7cce8ed195
adilson carvalho <adilsonsantanadecarvalho@gmail.com>
2020-10-01 16:20:29 -0300

    create action and view for create

--- app/controllers/submitted_texts_controller.rb
+++ app/controllers/submitted_texts_controller.rb
@@ -9,6 +9,17 @@ class SubmittedTextsController < ApplicationController
     @submitted = SubmittedText.new
   end
 
+  def create
+    @submitted = SubmittedText.new(submitted_params)
+    @submitted.user_id = current_user.id
+    @submitted.save
+    if @submitted.save
+      redirect_to submitted_texts(@submitted)
+    else
+      render :new
+    end
+  end
+
 
 
 
@@ -21,5 +32,15 @@ class SubmittedTextsController < ApplicationController
     if TranslatedText.all.select { |translation| translation.submitted_text_id == @text.id}.size > 0
       @reviews = TranslatedText.all.select { |translation| translation.submitted_text_id }
     end
+  end
+
+private
+
+  def set_submitted
+    @submitted = Submitted.find(params[:id])
+  end
+
+  def submitted_params
+    params.require(:submitted_text).permit(:url, :institution, :deadline, :service_title, :service, :target_public, :service_stages, :more_info)
   end
 end
