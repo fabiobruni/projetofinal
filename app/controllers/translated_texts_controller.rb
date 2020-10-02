187a3357d160074bb84e9af4bd878f9746e679bc
Fabio Bruni <fabiobruni@gmail.com>
2020-10-02 12:03:55 -0300

    definindo as actions

--- app/controllers/translated_texts_controller.rb
+++ app/controllers/translated_texts_controller.rb
@@ -1,4 +1,25 @@
 class TranslatedTextsController < ApplicationController
+  before_action :set_submitted, only: [:new, :create]
+
+  def index
+    @translated = TranslatedText.all
+  end
+
+
+  def new
+    @translated = TranslatedText.new
+  end
+
+  def create
+    @translated = TranslatedText.new(translated_params)
+    @translated.service_title = @submitted
+
+    if @translated.save
+      redirect_to submitted_text_path(@submitted)
+    else
+      render :new
+    end
+  end
 
   def show
     @review = TranslatedText.find(params[:id])
@@ -9,7 +30,15 @@ class TranslatedTextsController < ApplicationController
     @original = SubmittedText.find(@review.submitted_text_id)
   end
 
-  def index
-    @translated = TranslatedText.all
+ 
+
+  private
+  def set_submitted
+    @submitted = SubmittedText.find(params[:id])
   end
+
+  def translated_params
+    params.require(:translated_text).permit(:url, :institution, :deadline, :service_title, :service, :target_public, :service_stages, :more_info)
+  end
+
 end
