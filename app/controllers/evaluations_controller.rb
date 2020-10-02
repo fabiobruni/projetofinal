3fc1c2df40d8b2fad64b8778aad8adcdc609152a
adilson carvalho <adilsonsantanadecarvalho@gmail.com>
2020-10-02 12:14:45 -0300

    add new and create to evaluation

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,2 +1,29 @@
 class EvaluationsController < ApplicationController
+before_action :set_translated_text, only: [:new, :create]
+
+def new
+  @evaluation = Evaluation.new
+end
+
+def create
+    @evaluation = Evaluation.new(evaluation_params)
+    @evaluation.translated_text = @evaluation
+
+    if @evaluation.save
+      redirect_to evaluation_path(@evaluation)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_translated_text
+    @translated_text = Translated_text.find(params[:translated_text_id])
+  end
+
+  def evaluation_params
+    params.require(:evaluation).permit(:content, :stars)
+  end
+
 end
