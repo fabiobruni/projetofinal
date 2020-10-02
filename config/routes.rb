c0422535f5c4a8b7682ce57142230184f4c2d8ee
adilson carvalho <adilsonsantanadecarvalho@gmail.com>
2020-10-02 15:58:11 -0300

    set routes to begin in submitted_texts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'submitted_texts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  
+
+
   resources :submitted_texts, only: [ :index, :edit, :new, :create, :destroy, :show ] do
     resources :translated_texts, only: [ :index, :new, :create, :edit, :update , :show] do
       resources :evaluations, only: [ :new, :show, :edit, :update ]
