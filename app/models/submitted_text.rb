6176af060877ca9a5bb883a67e99d2f4d2151415
adilson carvalho <adilsonsantanadecarvalho@gmail.com>
2020-10-02 08:52:19 -0300

    fix search feature

--- app/models/submitted_text.rb
+++ app/models/submitted_text.rb
@@ -1,7 +1,11 @@
 class SubmittedText < ApplicationRecord
   belongs_to :user
   has_many :translated_texts, dependent: :destroy
-  include PgSearch::Model
-  multisearchable against: [:url, :institution, :deadline, :service_title, :service, :target_public, :service_stages, :more_info]
 
+  include PgSearch::Model
+  pg_search_scope :search_by_multiple_fields,
+    against: [:institution, :service_title, :service, :target_public],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
