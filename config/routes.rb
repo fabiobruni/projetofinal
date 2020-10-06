9864c7612bba403dcca5e1989f39bac32552d9c8
Marcos Flores <marcoschines@gmail.com>
2020-10-06 19:55:14 -0300

    edit do translated, correção das sugestões aceitas não serem mais vistas

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'submitted_texts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
    resources :submitted_texts, only: [ :index, :edit, :new, :create, :destroy, :show, :update ] do
-    resources :translated_texts, only: [ :index, :new, :create, :edit, :update , :show, :destroy] do
+    resources :translated_texts, only: [ :index, :new, :edit, :update, :create, :show, :destroy] do
+    # post 'edit', to: 'translated_texts#update'
       resources :evaluations, only: [:new, :create]
     end
   end
