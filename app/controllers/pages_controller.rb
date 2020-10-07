6dc4e4c161d28dd7acf898570631f02d81648d98
Marcos Flores <marcoschines@gmail.com>
2020-10-07 11:25:18 -0300

    rascunho da revisão antes de postar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,8 @@ class PagesController < ApplicationController
   def mypage
     @mysubmitted = SubmittedText.all.select { |submitted| submitted.user_id == current_user.id }
     @mytranslated = TranslatedText.all.select { |translated| translated.user_id == current_user.id}
-    @accepted = @mytranslated.select { |text| text.choosen_translat == true}
+    @myposted = @mytranslated.reject { |post| post.draft == true }
+    @mydraft = @mytranslated.select { |post| post.draft == true }
+    @accepted = @mytranslated.select { |text| text.choosen_translat == true }
   end
 end
